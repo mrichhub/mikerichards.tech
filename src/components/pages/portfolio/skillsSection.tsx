56c6c302fd4aa7cf4e52b5f88ba62873bb17ca7b
Mike Richards <bitbucket@mikemrichards.com>
2023-08-15 14:07:10 -0400

    Updated skills

--- src/components/pages/portfolio/skillsSection.tsx
+++ src/components/pages/portfolio/skillsSection.tsx
@@ -1,5 +1,5 @@
 import leaderIcon from "../../../assets/images/leader-icon.png"
-import skillsIcon from "../../../assets/images/skills-icon.png"
+import softwareDevelopmentIcon from "../../../assets/images/software-development-icon.png"
 import { AnimateInWhenVisible } from "../../animateInWhenVisible/animateInWhenVisible"
 import { TypewriterWhenVisible } from "../../typewriter/typewriterWhenVisible"
 import "./skillsSection.scss"
@@ -12,40 +12,13 @@ export function SkillsSection() {
 					<div className="item">
 						<div className="header">
 							<div className="icon">
-								<img src={leaderIcon} alt="Leader" />
+								<img src={softwareDevelopmentIcon} alt="Software Development" />
 							</div>
 							<div className="headline">
-								<h4><TypewriterWhenVisible text="Tech" delay={500} /></h4>
+								<h4><TypewriterWhenVisible text="Languages" delay={500} /></h4>
 							</div>
 						</div>
-						<p>
-							Experienced with leading technology development in growth stage companies, as well as getting heads down to get code done.
-						</p>
-						<p>
-							Tech is my passion, it's what I do in my free time, and I like delivering quality products that make people happy.
-						</p>
-					</div>
-				</AnimateInWhenVisible>
-
-				<AnimateInWhenVisible delay={500}>
-					<div className="item">
-						<div className="header">
-							<div className="icon">
-								<img src={skillsIcon} alt="Skills" />
-							</div>
-							<div className="headline">
-								<h4><TypewriterWhenVisible text="Skills" delay={750} /></h4>
-							</div>
-						</div>
-						<em className="note">
-							These are the skills I'm most proficient at. I'm open to all technologies and continually furthering my knowledge.
-						</em>
 						<ul>
-							<li>
-								<h5>React</h5>
-								<em>(This site developed in React)</em>
-								<a href="https://github.com/mrichhub/mikerichards.tech" target="_blank">See code on GitHub</a>
-							</li>
 							<li>
 								<h5>iOS</h5>
 								Swift, Obj-C
@@ -53,6 +26,11 @@ export function SkillsSection() {
 							<li>
 								<h5>Node.js</h5>
 								Express, Socket.io
+							</li>
+							<li>
+								<h5>React</h5>
+								<em>(This site developed in React)</em>
+								<a href="https://github.com/mrichhub/mikerichards.tech" target="_blank">See code on GitHub</a>
 							</li>
 							<li>
 								<h5>Typescript</h5>
@@ -77,6 +55,38 @@ export function SkillsSection() {
 								React, Redux, Angular, etc.
 							</li>
 						</ul>
+						<em className="note">
+							This is a snaptshot of the languages I'm currently most proficient at. I'm always open to all languages and continually furthering my knowledge.
+						</em>
+					</div>
+				</AnimateInWhenVisible>
+
+				<AnimateInWhenVisible delay={500}>
+					<div className="item">
+						<div className="header">
+							<div className="icon">
+								<img src={leaderIcon} alt="Leader" />
+							</div>
+							<div className="headline">
+								<h4><TypewriterWhenVisible text="Roles" delay={750} /></h4>
+							</div>
+						</div>
+						
+						<h5>Architect</h5>
+						<p>
+							I design systems around business use cases, navigate through complexities, and research to help establish solid technical foundations.
+						</p>
+						
+						<h5>Lead</h5>
+						<p>
+							I lead through agile processes, fostering an environment where engineers can take ownership and pride of features, while collaborating
+							with business stakeholders to ensure proper alignment.
+						</p>
+
+						<h5>Coder</h5>
+						<p>
+							Coding is my passion; it's what I do professionally and in my free time. I like getting heads down into code.
+						</p>
 					</div>
 				</AnimateInWhenVisible>
 
