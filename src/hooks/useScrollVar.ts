19e5f21152da10c37bb3e55b6f4d078956f93369
Mike Richards <bitbucket@mikemrichards.com>
2023-08-15 13:18:45 -0400

    Ensuring useScrollVar hook runs on init

--- src/hooks/useScrollVar.ts
+++ src/hooks/useScrollVar.ts
@@ -4,9 +4,17 @@ import { usePageScroll } from "./usePageScroll"
 export function useScrollVar(): number {
 	const scroll = usePageScroll()
 
-	useEffect(() => {
+	function setScrollProperty(scroll: number) {
 		document.documentElement.style.setProperty("--scroll", `${scroll * 100}`)
+	}
+
+	useEffect(() => {
+		setScrollProperty(scroll)
 	}, [scroll])
+
+	useEffect(() => {
+		setScrollProperty(scroll)
+	}, [])
 
 	return scroll
 }
